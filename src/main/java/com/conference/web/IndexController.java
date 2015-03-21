7010ec1fb18bc586136bf96ee33f9190367b3fc0
Artur Chernov <kavabungo1@gmail.com>
2015-03-22 01:48:08 +0500

    add index template and Room service Test

--- src/main/java/com/conference/web/IndexController.java
+++ src/main/java/com/conference/web/IndexController.java
@@ -1,42 +1,29 @@
 package com.conference.web;
 
-import com.conference.domain.Presentation;
 import com.conference.domain.Room;
-import com.conference.domain.User;
 import com.conference.service.PresentationRepository;
 import com.conference.service.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class IndexController {
 
     @Autowired
     private RoomRepository roomRepository;
+
     @Autowired
     private PresentationRepository presentationRepository;
 
     @RequestMapping("/")
-    @ResponseBody
-    public String getRooms(){
-        StringBuilder ans = new StringBuilder("");
-        Iterator<Room> rooms = roomRepository.findAll().iterator();
-        while(rooms.hasNext()){
-            Room room = rooms.next();
-            ans.append(room.getName()).append(" [ ");
-            for(Presentation presentation : room.getPresentations()){
-                ans.append(presentation.getName()).append(" ( ");
-                for(User user : presentation.getUsers()){
-                    ans.append(user.getEmail()).append("  ");
-                }
-                ans.append(" ) ");
-            }
-            ans.append(" ] \n");
-        }
-        return ans.toString();
+    public String getRooms(Model model){
+        model.addAttribute("rooms", roomRepository.findAll());
+        return "index";
     }
 }
---
+++ src/test/com/conference/service/RoomRepositoryTest.java
@@ -0,0 +1,27 @@
+package com.conference.service;
+
+import com.conference.ConferenceApplication;
+import com.conference.domain.Room;
+import junit.framework.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ConferenceApplication.class)
+public class RoomRepositoryTest {
+
+    @Autowired
+    RoomRepository roomRepository;
+
+    @Test
+    public void findFirstRoom(){
+        Room firstRoom = this.roomRepository.findOne(1L);
+        assertEquals(firstRoom.getName(), "Red room");
+    }
+
+}
